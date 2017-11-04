2a7363cc302c95c47a4d0001924ed5585782fe82
KURO\jk <ninelives82@hotmail.com>
2017-11-04 02:49:59 +0200

    Added c++ class not getting called..

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -1,7 +1,17 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankPlayerController.h"
+#include "BattleTank.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
 
+	UE_LOG(LogTemp, Warning, TEXT("TankPlayerController c++ class BeginPlay override!"))
+}
 
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
 
